ca18b5fd65ef62ba7773b9ac4608848275579b35
Kamil Tyszkiewicz <kamil.tyszkiewicz47@gmail.com>
2021-04-22 22:47:54 +0200

    Finish registration without sql yet

---
+++ app/src/main/java/com/digir/firebaseseriesmini/activites/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.digir.firebaseseriesmini.activites
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.digir.firebaseseriesmini.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/digir/firebaseseriesmini/registration/RegistrationFragment.kt
@@ -0,0 +1,53 @@
+package com.digir.firebaseseriesmini.registration
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.digir.firebaseseriesmini.BaseFragment
+import com.digir.firebaseseriesmini.R
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_sign_up.*
+
+class RegistrationFragment : BaseFragment() {
+
+    private val fbAuth = FirebaseAuth.getInstance()
+    private val REG_DEBUG = "REG_DEBUG"
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupSignUpClick()
+    }
+    private fun setupSignUpClick() {
+        signUpButtonRegistration.setOnClickListener {
+            val email = email_registration.text?.trim().toString()
+            val pass = pass_registration.text?.trim().toString()
+            val repeatPass = repeat_pass_registration.text?.trim().toString()
+
+            if(pass==repeatPass){
+                fbAuth.createUserWithEmailAndPassword(email, pass)
+                    .addOnSuccessListener {authRes ->
+                        if(authRes.user != null){
+                            startApp()
+                        }
+                    }
+                    .addOnFailureListener{exc ->
+                        Snackbar.make(requireView(), "Upss...Something went wrong...", Snackbar.LENGTH_SHORT)
+                            .show()
+                        Log.d(REG_DEBUG, exc.message.toString())
+                    }
+            }
+        }
+    }
+}
